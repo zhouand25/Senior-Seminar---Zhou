9b8881a98f67147a80cfb0e8ce13d31e501b368d
zhouand25 <124077648+zhouand25@users.noreply.github.com>
2024-01-22 13:07:38 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,3 +1,10 @@
-//Brainstormed rough ideas and outline, revised (had the wrong structure)
-//popularity ranking of classes system
-//Start of file
+import java.util.Scanner;
+public class Tester {
+  public static void main(String[] args) {
+  System.out.print("\nFile Name: ");
+  Scanner filename = new Scanner(System.in);
+  String file = filename.nextLine();
+  filename.close();
+  Seminar sem1 = new Seminar(file);
+  }
+}
