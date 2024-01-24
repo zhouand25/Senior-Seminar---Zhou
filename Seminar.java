6d346930255012d62f78798e90d3453acaddf7c1
zhouand25 <124077648+zhouand25@users.noreply.github.com>
2024-01-24 13:42:09 -0500

    Update Seminar.java

--- Seminar.java
+++ Seminar.java
@@ -18,30 +18,29 @@ public class Seminar {
         String data = s1.nextLine();
         String[] array = data.split(",");
         //names
-        names.add(array[3]);
+        names.add(array[2]);
         //courses
-        if(array[16]!="") {
+        System.out.println("STUFF: "+array[16].charAt(0));
+        if(array[16].charAt(0)!=' ') {
             courses.add(array[16]);
         }
         //choices
         int temp[] = new int[5];
         for(int i=0; i<5; ++i) {
-            String place = array[i+10];
-            if(place.substring(0,1)==" ") {
-                temp[i] = Integer.parseInt(array[i+10].substring(1,2));
+            String place = array[i+9];
+            if(place.charAt(0)==' ') {
+                temp[i] = Integer.parseInt(place.substring(1,2));
             } else {
-                temp[i] = Integer.parseInt(array[i+10].substring(0,2));
+                temp[i] = Integer.parseInt(place.substring(0,2));
             }
         }
-        choices.add(temp);
-        
-        for(int i=0; i<choices.size(); ++i) {
-            for(int j=0; j<5; ++j) {
-                System.out.print(choices.get(i)[j]+" ");
-            }
-            System.out.println("");
+        choices.add(temp);        
+      }
+      for(int i=0; i<choices.size(); ++i) {
+        for(int j=0; j<5; ++j) {
+            System.out.print(choices.get(i)[j]+" ");
         }
-        
+        System.out.println("");
       }
       s1.close();
     } catch (FileNotFoundException e) {
